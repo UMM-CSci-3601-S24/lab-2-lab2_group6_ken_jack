bb250e01d44793ea4eaae01d144d9ec47375a992
Ken Broden <brod0268@morris.umn.edu>
2024-01-28 17:45:52 -0600

    CHECKSTYLE FIX

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -2,7 +2,6 @@ package umm3601.todo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
